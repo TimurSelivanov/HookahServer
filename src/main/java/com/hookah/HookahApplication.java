65f6db579323ac119c567365b0650baba32cc1db
TimSelivan <timselivanson@gmail.com>
2023-09-09 18:50:41 +0300

    git init

---
+++ src/main/java/com/hookah/HookahApplication.java
@@ -0,0 +1,13 @@
+package com.hookah;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HookahApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HookahApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hookah/HookahApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hookah;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HookahApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
